adada6bab113124c8a3ec9b5e0a5c116a0536813
PrinceLavel <prts00001@stud.uni-saarland.de>
2025-06-17 14:03:19 +0200

    Comments removed

--- socialnetwork/api.py
+++ socialnetwork/api.py
@@ -6,7 +6,6 @@ from socialnetwork.models import Posts, SocialNetworkUsers
 
 # general methods independent of html and REST views
 # should be used by REST and html views
-#Hi it's still me
 
 def _get_social_network_user(user) -> SocialNetworkUsers:
     """Given a FameUser, gets the social network user from the request. Assumes that the user is authenticated."""
