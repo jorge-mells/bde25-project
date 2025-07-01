7334c1345c946a03ef804e98105c8e57371e9cac
PrinceLavel <prts00001@stud.uni-saarland.de>
2025-07-01 11:08:15 +0200

    Update urls.py
    T6

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -4,6 +4,7 @@ from socialnetwork.views.html import timeline
 from socialnetwork.views.html import follow
 from socialnetwork.views.html import unfollow
 from socialnetwork.views.rest import PostsListApiView
+from socialnetwork.views.html import bullshitters    #T6
 
 app_name = "socialnetwork"
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path("html/timeline", timeline, name="timeline"),
     path("api/follow", follow, name="follow"),
     path("api/unfollow", unfollow, name="unfollow"),
+    path("html/bullshitters", bullshitters_view, name="bullshitters"),   #T6
 ]
